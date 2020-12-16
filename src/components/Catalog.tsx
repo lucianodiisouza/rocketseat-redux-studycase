3f889a9c6ad6db14e8cef060db1ad60628347f2d
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-12-16 17:33:44 -0300

    adding json-server, initialize axios, create api call for products

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,29 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { IProduct } from "../store/modules/cart/types";
 
-function Catalog() {
-  const catalog = useSelector((state) => state);
-  return <h1>HelloWorld</h1>;
-}
+import api from "../services/api";
+
+const Catalog: React.FC = () => {
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get("products").then((res) => {
+      setCatalog(res.data);
+    });
+  }, []);
+  return (
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span>
+          {"  "}
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
+};
 
 export default Catalog;
